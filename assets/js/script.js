16d96cc84bd1facd0c3bf8d8505e942e771af31e
Dawan Sawyer <dawansawyer@gmail.com>
2021-03-19 02:55:02 -0400

    Created a function to see if the searched city exists in the entries

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,15 @@ var currentHumidty = $("#humidity");
 var currentWSpeed = $("#wind-speed");
 var currentUvindex = $("#uv-index");
 var sCity = [];
+var APIKey = "a0aca8a89948154a4182dcecc780b513";
+
+// function that searches the city to see if it exists in the entries
+function find(city) {
+  for (var i = 0; i < sCity.length; i++) {
+    if (city.toUpperCase() === sCity[i]) {
+      return -1;
+    }
+  }
+  return 1;
+}
+
