db20b1b49ffc2d934f8bde13669df1f00de650ae
Dawan Sawyer <dawansawyer@gmail.com>
2021-03-19 03:45:13 -0400

    Created a statement to return the UVindex response, the first commit didnt have this

--- assets/js/script.js
+++ assets/js/script.js
@@ -87,3 +87,15 @@ function currentWeather(city) {
     }
   });
 }
+
+// function that will return the UVindex response
+function UVIndex(ln, lt) {
+  var uvqURL =
+    "https://api.openweathermap.org/data/2.5/uvi?appid=" + APIKey + "&lat=" + lt + "&lon=" + ln;
+  $.ajax({
+    url: uvqURL,
+    method: "GET",
+  }).then(function (response) {
+    $(currentUvindex).html(response.value);
+  });
+}
